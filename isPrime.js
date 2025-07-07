a8d2e7d762f8f5c178b4b9488b0ed3bd33895d5f
aboudev22 <aboubakarjay2@gmail.com>
2025-07-07 11:38:52 +0100

    feat(optimisation) - add 6k + 1 verification

--- isPrime.js
+++ isPrime.js
@@ -1,5 +1,5 @@
 /**
- *
+ * Primaly test
  * @param {number} n - The integer to test.
  * @returns {Boolean} True if the number is prime, false otherwise.
  */
@@ -9,8 +9,8 @@ function isPrime(n) {
 
   if (n % 2 === 0 || n % 3 === 0) return false;
 
-  for (let i = 2; i * i <= n; i = i + 6) {
-    if (n % i === 0) return false;
+  for (let i = 5; i * i <= n; i = i + 6) {
+    if (n % i === 0 || n % (i + 2) === 0) return false;
   }
   return true;
 }
